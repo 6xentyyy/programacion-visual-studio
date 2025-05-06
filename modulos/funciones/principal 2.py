1c56b513d5a7d6be3c338a6fb0ca75e7cf13b904
6xentyyy <6xpk123@gmail.com>
2025-05-06 18:27:27 -0400

    yepa k pasa xavales

---
+++ modulos/funciones/principal 2.py
@@ -0,0 +1,27 @@
+import funciones.cuadrilatero
+
+def menu_principal():
+    print()
+    print("Cálculo de funciones geometricas.")
+    print("1: parametro")
+    print("2: Área")
+    print("3: Volumen")
+    print("0: salir")
+    print()
+ 
+   while True:
+       opcion = input("Seleccione su opción (0-3): ")
+       if opcion == "1":
+         ancho
+         pass
+       elif opcion == "2":
+           pass
+       elif opcion == "3":
+           pass
+       elif opcion == "0":
+           print("Saliendo del sistema...")
+           break
+       else:
+           print("opción invalida...")
+
+menu_principal()
