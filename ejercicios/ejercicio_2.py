14358d2d7d9e0bd83175cc3d36ae0e300c48c21b
6xentyyy <6xpk123@gmail.com>
2025-05-20 16:37:41 -0400

    lolololololo

---
+++ ejercicios/ejercicio_2.py
@@ -0,0 +1,5 @@
+lista_peliculas = ["Destino FInal","Eternauta","Lilo y Stich","Los Piratas del Caribe","El bueno, el malo y el feo"]
+num = 1
+for x in lista_peliculas:
+    print(f"pelicula {num}: {x}")
+    num +=1
